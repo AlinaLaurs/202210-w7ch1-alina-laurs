1c7d8389b98524930fdb121bf61035a0da83753c
Alina <alilaursdesign@gmail.com>
2022-11-15 18:56:44 +0100

    error test

--- src/interfaces/error.test.ts
+++ src/interfaces/error.test.ts
@@ -5,14 +5,14 @@ describe('Given', () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let error: CustomError;
     beforeEach(() => {
-        error = new HTTPError(418, 'Tea Pot', 'Ja, ja, ja');
+        error = new HTTPError(400, 'Bad', 'Bad Request');
     });
     test('should first', () => {
         expect(error).toBeInstanceOf(Error);
         expect(error).toBeInstanceOf(HTTPError);
-        expect(error).toHaveProperty('statusCode', 418);
-        expect(error).toHaveProperty('statusMessage', 'Tea Pot');
-        expect(error).toHaveProperty('message', 'Ja, ja, ja');
+        expect(error).toHaveProperty('statusCode', 400);
+        expect(error).toHaveProperty('statusMessage', 'Bad');
+        expect(error).toHaveProperty('message', 'Bad Request');
         expect(error).toHaveProperty('name', 'HTTPError');
     });
 });

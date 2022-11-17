926c6fe25175ae37f34c12fe0a39328179c030ae
Alina <alilaursdesign@gmail.com>
2022-11-17 18:18:40 +0100

    Add new interface

---
+++ src/interfaces/tapa.ts
@@ -0,0 +1,14 @@
+export type ProtoTapa = {
+    name: string;
+    origin: string;
+    ingredient: string;
+    tasted: boolean;
+};
+
+export type Tapa = {
+    id: number;
+    name: string;
+    origin: string;
+    ingredient: string;
+    tasted: boolean;
+};

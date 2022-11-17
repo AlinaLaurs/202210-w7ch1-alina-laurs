6e8d7f0b80234203bed3d7893436b861cee25385
Alina <alilaursdesign@gmail.com>
2022-11-17 20:27:00 +0100

    Add db.conect.test.ts

--- src/db.conect.ts
+++ src/db.conect.ts
@@ -5,7 +5,5 @@ export function dbConnect() {
     const DBName = process.env.NODE_ENV !== 'test' ? 'tapas' : 'TapasTesting';
     let uri = `mongodb+srv://${USER1}:${PASSW}`;
     uri += `@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
-    console.log({ DBName });
-    console.log({ uri });
     return mongoose.connect(uri);
 }

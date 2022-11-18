8b4f42c016125b4b67e30c77a087399d60efc4c6
Alina <alilaursdesign@gmail.com>
2022-11-18 20:44:47 +0100

    Adding SonarCloud env

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-    res.send('API de tapas').end();
+    res.send('API express de tapas').end();
 });
 
 app.use('/tapas', tapaRouter);
